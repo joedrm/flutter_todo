ec72709e58516a7f8cff7e8b8c819cf6863e8d1f
joe <wf_pinbo@163.com>
2022-12-05 16:16:17 +0800

    修复url中#问题

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "d83a1319c6e824a6ea037b00e308c04f",
-"index.html": "2a8ccd9d6cf3252a85812a3b8b3083f4",
-"/": "2a8ccd9d6cf3252a85812a3b8b3083f4",
-"main.dart.js": "fe1d6d61b5d2cd943f5e4313c356e9aa",
+"index.html": "0738eb0985443ca6edd9e576741a07ec",
+"/": "0738eb0985443ca6edd9e576741a07ec",
+"main.dart.js": "8d7fca2f8a72b35f43abd9b60388fd61",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
@@ -14,7 +14,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "a141879cf75a4c286518328b7d0219ed",
 "assets/AssetManifest.json": "f94816fdc3113f28035e726125483946",
-"assets/NOTICES": "77d65cc72427e277c0b78428b01ec3fa",
+"assets/NOTICES": "b4c2e60d121166863d677c025ddff0d8",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:url_strategy/url_strategy.dart';
+// import 'package:url_strategy/url_strategy.dart';
 
 import 'app.dart';
 
 void main() {
-  setPathUrlStrategy();
+  // setPathUrlStrategy();
   runApp(App());
 }
